a87fc837b26f04a8a71f4756e13efaf1611a2439
Herbet Dias Alves Reis <herbet08@hotmail.com>
2023-03-26 16:26:42 -0300

    Login parameters

---
+++ processa_registro.php
@@ -0,0 +1,23 @@
+<?php
+include 'conexao.php';
+
+// Verifica se o formulário foi submetido
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  // Processa os dados do formulário
+  $nome = $_POST["nome"];
+  $email = $_POST["email"];
+  $senha = $_POST["senha"];
+
+  // Insere os dados do usuário na tabela
+  $inserir_usuario = "INSERT INTO usuarios (nome, email, senha) VALUES ('$nome', '$email', '$senha')";
+  
+  if (mysqli_query($conn, $inserir_usuario)) {
+    echo "Usuário cadastrado com sucesso!";
+  } else {
+    echo "Erro ao cadastrar usuário: " . mysqli_error($conn);
+  }
+
+  // Fecha a conexão com o banco de dados
+  mysqli_close($conn);
+}
+?>
